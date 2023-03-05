5049b9340e3e8ccf22e4fcc82d69319437b11ca3
Pong-son <kkamo127327@gmail.com>
2023-03-05 13:21:14 +0800

    add exercise

---
+++ JSF001/002.js
@@ -0,0 +1,5 @@
+const sentence = "Tecky Academy is the best coding school in Hong Kong"
+
+console.log(sentence.slice(0,6))
+console.log(sentence.slice(6,13))
+console.log(sentence.slice(0,13).toLowerCase(),sentence.slice(14,sentence.length-10),sentence.slice(sentence.length-9).toUpperCase())
